e95ec8a1ccaf85534177ac8cfec5860fd1ad1d7c
Alcan-Phoenix <alc.anouar@gmail.com>
2018-11-25 15:13:13 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> extList = GetExtensions(
+									$@"{Directory.GetCurrentDirectory()}\folder\", "*.*");
+			extList.ForEach(Console.WriteLine);
+		}
+
+		public static List<string> GetExtensions(string path, string file)
+		{
+			DirectoryInfo directory = new DirectoryInfo(path);
+			FileInfo[] Files = directory.GetFiles(file);
+			List<string> extList = new List<string>();
+			foreach (FileInfo f in Files)
+			{
+				extList.Add(Path.GetExtension(f.ToString()));
+			}
+			List<string> extensions = extList.Distinct().ToList();
+			return extensions;
+		}
+	}
+}
